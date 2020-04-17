119cdea9374a2faed87e8cf2335b48489857b66e
Pablo Menz <p.menz.r@gmail.com>
2020-04-17 02:58:04 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,5 +1,5 @@
 module FancyDance
-  
+  module InstanceMethods
  
     def twirl
       "I'm twirling!"
@@ -16,12 +16,12 @@ module FancyDance
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
-  
+  end
  
-
+  module ClassMethods
  
     def metadata
       "This class produces objects that love to dance."
-    
+    end
   end
 end
